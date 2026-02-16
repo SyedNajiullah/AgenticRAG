78f58baf7085b4ec288a1310abee7d9910b08c31
SyedNajiullah <najiullahsyed2@gmail.com>
2026-02-16 14:32:49 +0500

    updated models.py file by adding langgraph messages state

--- app/llms/models.py
+++ app/llms/models.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, Field
-from typing import Literal
+from typing import TypedDict, Annotated, List, Literal
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
 
 class GreetingClassification(BaseModel):
     category: Literal["greeting", "non_greeting"] = Field(
@@ -15,3 +17,11 @@ class AnswerSatisfactionClassification(BaseModel):
     category: Literal["yes", "no"] = Field(
         description="Whether the message is yes or no"
     )
+
+class ChatState(TypedDict):
+    question: str # question from the user
+    messages: Annotated[List[BaseMessage], add_messages] # all messages history fot short term memory.
+    phase: str # current phase of the chat web or rag
+    rag_context: str # context from the rag
+    web_context: str # context from the web
+    ltm_context: str # context from long term memory
